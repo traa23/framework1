7802641e2d9c9cbdf0f48896c25f0be9a04acb0a
putra <anugrah24si@mahasiswa.pcr.ac.id>
2025-09-30 20:18:59 +0700

    First commit...

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mahasiswa/{param1}', function () {
+    return "Mahasiswa epep";
+});
+
+Route::get('/nama/{param1}', function ($param1){
+    return "Nama saya : ".$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
